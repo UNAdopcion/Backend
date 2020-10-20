14b213541716d2974bca3df902d1f6196f9e6dc8
Nicolas Rodriguez <nirodriguezsa@unal.edu.co>
2020-10-19 22:46:04 -0500

    Pestaña denuncia agregada

--- unadopcion/src/main/resources/src/views/Denuncia.vue
+++ unadopcion/src/main/resources/src/views/Denuncia.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>SECCION DE DENUNCIAS</h1>
+  <div>
+    <Header></Header>
+    <h1>SECCION DE DENUNCIAS</h1>
+    <Denuncia></Denuncia>
+    <Footer></Footer>
+  </div>
+  
 </template>
 
 <script>
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Denuncia from "../components/ComponentDenuncia";
+
 export default {
-  name: "Denuncia"
+  name: "Principal",
+  components: {
+    Header, Footer, Denuncia
+  }
 }
 </script>
 
