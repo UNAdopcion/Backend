8e1bca3fb2a7255e5ac43fbc09ef788421ac7524
etrobertot <etrobertot@unal.edu.co>
2020-11-05 12:37:36 -0500

    Eliminacion migración erronea V1_4_nuevo_constraint_adopción

--- unadopcion/src/main/resources/db/migration/V1_4__Adicion_Columna_Usuario.sql
+++ unadopcion/src/main/resources/db/migration/V1_4__Adicion_Columna_Usuario.sql
@@ -1,2 +1,8 @@
 ALTER TABLE usuario
 ADD COLUMN usuario_nombre_real varchar(255) not null;
+
+ALTER TABLE usuario
+ADD COLUMN usuario_google_id varchar(255) not null;
+
+ALTER TABLE usuario
+ADD COLUMN usuario_url_foto varchar(5000) not null;
