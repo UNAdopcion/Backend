5f5f7cafe62b2d34f8bc90afd7e4181bb965354e
Edprietov <edprietov@unal.edu.co>
2020-12-03 00:41:40 -0500

    Update V1_5__Adicion_Columna_Tabla_Animal.sql

--- unadopcion/src/main/resources/db/migration/V1_5__Adicion_Columna_Tabla_Animal.sql
+++ unadopcion/src/main/resources/db/migration/V1_5__Adicion_Columna_Tabla_Animal.sql
@@ -1,8 +1,1 @@
-ALTER TABLE animal
-ADD COLUMN anim_microchip_id int;
 
-ALTER TABLE animal
-ADD COLUMN anim_esterilizacion varchar(32) null;
-
-ALTER TABLE animal
-ADD COLUMN anim_estado varchar(32) null;
