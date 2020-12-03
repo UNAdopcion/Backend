0f19a80ecbcff207a0344cdd7378a02a52d233ab
Edprietov <edprietov@unal.edu.co>
2020-12-03 00:25:09 -0500

    Update V1_9__Correccion_Tabla_Solicitud.sql

--- unadopcion/src/main/resources/db/migration/V1_9__Correccion_Tabla_Solicitud.sql
+++ unadopcion/src/main/resources/db/migration/V1_9__Correccion_Tabla_Solicitud.sql
@@ -1,8 +1,1 @@
-ALTER TABLE solicitud
-ADD COLUMN personaid int not null;
 
-alter table solicitud add constraint fk_solicitud_Persona foreign key (personaid)
-        references  usuario(usuario_id) on delete restrict on update restrict;
-
-alter table solicitud add constraint fk_solicitud_Animal foreign key (animid)
-        references  animal(anim_id) on delete restrict on update restrict;
