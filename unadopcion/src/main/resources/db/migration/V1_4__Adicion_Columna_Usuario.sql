ef2b63a1cba344832b5d5eebf66e5f0e1f89d6c6
Edprietov <edprietov@unal.edu.co>
2020-12-03 00:18:29 -0500

    Update V1_4__Adicion_Columna_Usuario.sql

--- unadopcion/src/main/resources/db/migration/V1_4__Adicion_Columna_Usuario.sql
+++ unadopcion/src/main/resources/db/migration/V1_4__Adicion_Columna_Usuario.sql
@@ -1,11 +1,1 @@
-ALTER TABLE usuario
-ADD COLUMN usuario_nombre_real varchar(255) not null;
 
-ALTER TABLE usuario
-ADD COLUMN usuario_google_id varchar(255) not null;
-
-ALTER TABLE usuario
-ADD COLUMN usuario_url_foto varchar(5000) not null;
-
-ALTER TABLE usuario
-MODIFY COLUMN usuario_email varchar(400)
